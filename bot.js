87842e95b508e56858c8ad774524374fededdd33
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-03-06 22:09:45 -0800

    Basic bot functional test

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Commando = require("discord.js-commando");
+const bot = new Commando.Client({
+    owner: "351094569309306891",
+    commandPrefix: "!",
+    disableEveryone: true
+});
+const path = require("path");
+
+bot.registry
+    // Registers your custom command groups
+    .registerGroups([
+        ['raid', 'Raid commands']
+        //['some', 'Some group'],
+        //['other', 'Some other group']
+    ])
+
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
+
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+bot.on("ready", () => {
+    console.log(`Raid Manager is online. ${bot.user.username}`);
+    bot.user.setActivity("Raiding")
+});
+
+bot.login("");
