a4f49e9f24c37478bd695a817e72c220461f6054
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-03-06 22:34:14 -0800

    Add first test command

---
+++ commands/raid/reply.js
@@ -0,0 +1,18 @@
+const Commando = require("discord.js-commando");
+
+module.exports = class ReplyCommand extends Commando.Command {
+    constructor(bot) {
+        super(bot, {
+            name: "reply",
+            aliases: ["ping", "hello"],
+            group: "raid",
+            memberName: "reply",
+            description: "Replies with a message.",
+            examples: ["reply"]
+        });
+    }
+    
+    async run(msg) {
+        return msg.reply("Hi, I\'m awake!");
+    }
+};
