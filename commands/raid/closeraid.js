2ae8d13822bef3466485542354b00359b9ae57f3
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-03-08 20:49:24 -0800

    Finish closeraid command

--- commands/raid/closeraid.js
+++ commands/raid/closeraid.js
@@ -19,11 +19,6 @@ module.exports = class CloseRaidCommand extends Commando.Command {
                     key: "raid",
                     prompt: "Please enter the file name with the command.",
                     type: "string",
-                    // validate: text => {
-                    //     let m = text.split(" ");
-                    //     if (m[1]) return "Name for text file / name of raid, one word only."
-                    //     else return true;
-                    // }
                 }
             ]
         });
@@ -36,25 +31,12 @@ module.exports = class CloseRaidCommand extends Commando.Command {
 
         let raidChan = msg.guild.channels.find(c => c.name === "raid-channel");
         if (msg.channel.id === raidChan.id) {
-            if (!message) {
-                return msg.reply(`Raid signups now open for ${raid}!"`);
-            } else {
-                msg.reply(message);
-            }
-        } else {
-            return msg.reply(`Please use this command in ${raidChan} to sign up!`);
-        }
-
-        fs.open(`./${fileName}`, 'wx', (err, fd) => {
-            if (err) {
-                if (err.code === "EEXIST") {
-                    return msg.reply(`File Name ${fileName} already exists, use another name or clear the raid.`)
-                }
-                throw err;
-            }
-            fs.close(fd, (err) => {
-                if (err) throw err;
+            fs.unlink(`./${fileName}`, (err) => {
+                if (err) return msg.reply(`Error ${fileName} does not exist.`);
+                return msg.reply(`Deleted ${fileName} successfully.`)
             });
-        });
+        } else {
+            return msg.reply(`Please use this command in ${raidChan}`);
+        }
     }
 }
