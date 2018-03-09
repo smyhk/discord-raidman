85567066f334c5edfa9d76c6b4a02f6d68ed8c46
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-03-08 22:52:59 -0800

    Refactor file read

--- commands/raid/rolecall.js
+++ commands/raid/rolecall.js
@@ -24,34 +24,28 @@ module.exports = class RolecallCommand extends Commando.Command {
         });
     }
 
-    processRaidData(msg, raid, content) {
-        if (content === null) return `No users signed up for ${raid} raid.`;
-        for (player of content) {
-            // TODO: expand this after player signup is complete
-            return msg.reply(player);
-        }
-    }
-
     async run(msg, args) {
         msg.delete();  // delete original message
         let raid = args.raid;
         let fileName = args.raid + ".json"
-        let message = args.message;
         
         let raidChan = msg.guild.channels.find(c => c.name === "raid-channel");
         if (msg.channel.id === raidChan.id) {
-            fs.readFile(`./${fileName}`, (err, data) => {
-                if (err) {
-                    return msg.reply(`Error: ${fileName} does not exist.`);
+            if (fileName) {
+                let raiders = require(`./${fileMName}`);
+                for (raider of raiders) {
+                    let player = JSON.parse(player);
+                    return msg.reply(player);
                 }
+            } else {
+                return msg.reply(`Error: ${fileName} does not exist.`);
+            }
+            // fs.readFile(`./${fileName}`, (err, data) => {
+            //     if (err) return msg.reply(`Error: ${fileName} does not exist.`);
 
-                let content = data;
-                this.processRaidData(msg, raid, content);
+            //     let players = JSON.parse(data);
 
-                fs.close(fd, (err) => {
-                    if (err) throw err;
-                });
-            });
+            // });
         } else {
             return msg.reply(`Please use this command in ${raidChan}`);
         }
