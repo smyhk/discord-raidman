fcf141e584bd8bd2d8ea7d767c9d8edd7bc59587
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-03-16 14:51:35 -0700

    Finish status command

--- commands/raid/status.js
+++ commands/raid/status.js
@@ -1,5 +1,5 @@
 const Commando = require("discord.js-commando");
-const RichEmbed = require("discord.js");
+const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports = class StatusCommand extends Commando.Command {
@@ -29,10 +29,15 @@ module.exports = class StatusCommand extends Commando.Command {
         msg.delete();  // delete original message
         let raid = args.raid;
         let fileName = args.raid + ".json";
+        let dps = 0;
+        let tank = 0;
+        let heals = 0;
+        let raiders = [];
+        let  counter = 0;
         
         let raidChan = msg.guild.channels.find(c => c.name === "raid-channel");
         if (msg.channel.id === raidChan.id) {
-            fs.readFile(`./${fileName}`, (err, data) => {
+            fs.readFile(`./raids/${fileName}`, (err, data) => {
                 if (err) return msg.reply(`Error: ${fileName} does not exist.`);
 
                 let players = JSON.parse(data);
@@ -41,11 +46,34 @@ module.exports = class StatusCommand extends Commando.Command {
                 
                 for (let key in players) {
                     if (players.hasOwnProperty(key)) {
-                        msg.say(players[key].id);
+                        let role = players[key].role;
+                        switch (role) {
+                            case "Tank":
+                                tank++;
+                                break;
+                            case "Dps":
+                                dps++;
+                                break;
+                            case "Heals":
+                                heals++;
+                                break;
+                            default:
+                                msg.reply("Why are there no roles? Check your raid file.");
+                        }
+                        raiders += players[key].id + " ";
+                        counter++;
                     }
                 }
-                return msg.say(`Forming up for ${raid}! Time to log in.`);
+
+                const embed = new Discord.RichEmbed()
+                    .setDescription(`Status for ${raid} raid:`)
+                    .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+                    .setColor(0x00AE86)
+                    .addField("Players: ", raiders)
+                    .addField("Role and player count: ", 
+                        `${dps} dps, ${tank} tank(s), ${heals} healer(s) signed up. Total raiders: ${counter}`);
                 
+                return msg.embed(embed);
             });
         } else {
             return msg.reply(`Please use this command in ${raidChan}`);
