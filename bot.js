b2fe3a852e3958e279cf12018ace852305c798fa
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-01-21 17:05:35 -0800

    Update packages and do some minor refactoring

--- bot.js
+++ bot.js
@@ -1,40 +1,41 @@
-const botSettings = require("./botsettings.json");
-const Commando = require("discord.js-commando");
+const botSettings = require('./botsettings.json');
+const Commando = require('discord.js-commando');
+const path = require('path');
+const fs = require('fs');
+
 const bot = new Commando.Client({
-    owner: "351094569309306891",
-    commandPrefix: "!",
-    disableEveryone: true
+  owner: '351094569309306891',
+  commandPrefix: '!',
+  disableEveryone: true
 });
-const path = require("path");
-const fs = require("fs");
 
 bot.registry
-    // Registers your custom command groups
-    .registerGroups([
-        ['raid', 'Raid commands'],
-        ['social', 'Social commands']
-        //['other', 'Some other group']
-    ])
+  // Registers your custom command groups
+  .registerGroups([
+    ['raid', 'Raid commands'],
+    ['social', 'Social commands']
+    //['other', 'Some other group']
+  ])
 
-    // Registers all built-in groups, commands, and argument types
-    .registerDefaults()
+  // Registers all built-in groups, commands, and argument types
+  .registerDefaults()
 
-    // Registers all of your commands in the ./commands/ directory
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+  // Registers all of your commands in the ./commands/ directory
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
-bot.on("ready", () => {
-    console.log(`Raid Manager is online. ${bot.user.username}`);
-    bot.user.setActivity("Raiding")
+bot.on('ready', () => {
+  console.info(`Raid Manager is online. ${bot.user.username}`);
+  bot.user.setActivity('Raiding');
 
-    // create directory to hold raid files
-    fs.mkdir("./raids/", err => {
-        if (err) {
-            if (err.code === "EEXIST") return;
+  // create directory to hold raid files
+  fs.mkdir('./raids/', err => {
+    if (err) {
+      if (err.code === 'EEXIST') return;
 
-            console.log(err);
-            return;
-        }
-    });
+      console.error(err);
+      return;
+    }
+  });
 });
 
 bot.login(botSettings.token);
